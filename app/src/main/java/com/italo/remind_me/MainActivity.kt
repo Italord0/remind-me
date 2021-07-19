a667c8665b1377549de7b6dcba863198161dd9f0
Italo Melo <italord.melo@gmail.com>
2021-07-19 00:15:50 -0300

    First commit

---
+++ app/src/main/java/com/italo/remind_me/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.italo.remind_me
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
